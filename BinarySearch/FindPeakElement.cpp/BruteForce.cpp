88cab92f361055bd458188d1bdede1e3f5fe4543
Gaurav101 <gauravsingh74580@gmail.com>
2025-01-21 22:58:16 +0530

    First Commit

--- BinarySearch/FindPeakElement.cpp/BruteForce.cpp
+++ BinarySearch/FindPeakElement.cpp/BruteForce.cpp
@@ -1,28 +1,29 @@
-#include<bits/stdc++.h>
-using namespace std ;
+#include <bits/stdc++.h>
+using namespace std;
 
-int Result(vector<int> arr){
+int Result(vector<int> arr) {
     int n = arr.size();
-    int count = 0 ;
-    
-    if(n == 1)return 1 ;
+    int count = 0;
 
-    for(int i = 0 ; i < n ; i++){
-        if(i == 0 && arr[i] > arr[i + 1]) count++;
-        if(i == n - 1 && arr[n - 1] > arr[n - 2]) count++;
-        if(arr[i - 1] < arr[i] && arr[i] > arr[i + 1]){
+    if (n == 1) return 1; // A single element is always a peak
+
+    for (int i = 0; i < n; i++) {
+        if (i == 0 && arr[i] > arr[i + 1]) { // Check for the first element
+            count++;
+        } else if (i == n - 1 && arr[i] > arr[i - 1]) { // Check for the last element
+            count++;
+        } else if (i > 0 && i < n - 1 && arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) { // Check middle elements
             count++;
         }
     }
-    return count ;
+
+    return count;
 }
 
-int main(){
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 3, 6, 7, 8, 5, 1};
 
-vector<int> arr = {1 ,2 ,3 ,4 ,3 ,6, 7, 8, 5, 1};
+    cout << "The number of peak element is : " << Result(arr) << endl;
 
-cout<<"Result is "<<Result(arr);
-
-
-    return 0 ;
+    return 0;
 }
