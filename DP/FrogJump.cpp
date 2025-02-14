a70972893be68a2ee4ad63e407c1e8900c4f79c4
Gaurav101 <gauravsingh74580@gmail.com>
2025-02-14 18:20:44 +0530

    First commit

---
+++ DP/FrogJump.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+
+int minCost(int n , vector<int> &heights , vector<int> &dp){
+    if(n == 0) return 0 ;
+
+    if(dp[n] != -1) return dp[n];
+
+    int left = minCost(n - 1 , heights , dp) + abs(heights[n] - heights[n - 1]);
+    int right = INT_MAX ;
+
+    if(n > 1) right = minCost(n - 2 , heights , dp) + abs(heights[n] - heights[n - 2]);
+
+
+    return dp[n] = min(left , right);
+}
+
+int main(){
+
+    vector<int> heights = {20 , 30 , 40 , 20};
+
+    int n = heights.size();
+
+    vector<int> dp(n + 1 , -1);
+
+    cout<<"Cost required : "<<minCost(n - 1 , heights , dp);
+
+    return 0 ;
+}
