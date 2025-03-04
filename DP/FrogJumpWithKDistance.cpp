46e777810803c2f191db6257c37e885bcd54e35d
Gaurav101 <gauravsingh74580@gmail.com>
2025-03-04 23:10:45 +0530

    Linked List basic

--- DP/FrogJumpWithKDistance.cpp
+++ DP/FrogJumpWithKDistance.cpp
@@ -1,24 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int jump(vector<int> &arr, int n, int k) {
-    if (n == 1) return 0; 
+int jump(vector<int> &arr, int n, int k , vector<int>& dp) {
+    if (n == 0) return 0; 
+    if(dp[n] != -1) return dp[n] ;
     
     int path = INT_MAX;
 
     for (int j = 1; j <= k; j++) {
-        if (n - j >= 1) {  
-            int x = jump(arr, n - j, k) + abs(arr[n - 1] - arr[n - j - 1]);
+        if (n - j >= 0) {  
+            int x = jump(arr, n - j, k , dp) + abs(arr[n] - arr[n - j]);
             path = min(path, x);
         }
     }
     
-    return path;
+    return dp[n] = path;
 }
 
 int minimizeCost(int k, vector<int>& arr) {
-    int n = arr.size();
-    return jump(arr, n, k);
+    int n = arr.size() - 1;
+    vector<int> dp (n + 1 , -1);
+    return jump(arr, n, k , dp);
 }
 
 int main() {
@@ -27,3 +29,17 @@ int main() {
     cout << minimizeCost(k, arr) << endl;
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
