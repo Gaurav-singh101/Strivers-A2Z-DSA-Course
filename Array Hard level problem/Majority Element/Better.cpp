084a63fc97e0a155856e9d9416fc11f122688567
Gaurav101 <gauravsingh74580@gmail.com>
2025-04-06 23:25:12 +0530

    Load

--- Array Hard level problem/Majority Element/Better.cpp
+++ Array Hard level problem/Majority Element/Better.cpp
@@ -1,36 +1,35 @@
-// #include<iostream>
-// #include<bits/stdc++.h>
-// using namespace std ;
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std ;
 
-// list<int> Majority(int arr[] , int n){
-//     list <int> ls;
-//     int mm = (n / 3) + 1 ;
-//     map<int, int> map;
+list<int> Majority(int arr[] , int n){
+    list <int> ls;
+    map<int, int> map;
 
-//     for(int i = 0 ; i < n ; i++){
-//         map[arr[i]]++;
-//         if(map[arr[i]] == mm){
-//             ls.push_back(arr[i]);
-//         }
-//     }
-//     return ls ;
-// }
+    for(int i = 0 ; i < n ; i++){
+        map[arr[i]]++;
+        if(map[arr[i]] > (n / 3)){
+            ls.push_back(arr[i]);
+        }
+    }
+    return ls ;
+}
 
-// int main(){
+int main(){
 
-//     int n ;
-//     cout<<"Enter the array size : ";
-//     cin >> n;
-//     int arr[n] ;
-//     cout<<"Enter array element : ";
-//     for(int i = 0 ; i < n ; i ++){
-//         cin>>arr[i];
-//     }
+    int n ;
+    cout<<"Enter the array size : ";
+    cin >> n;
+    int arr[n] ;
+    cout<<"Enter array element : ";
+    for(int i = 0 ; i < n ; i ++){
+        cin>>arr[i];
+    }
 
-//     list<int> Result = Majority(arr , n);
+    list<int> Result = Majority(arr , n);
 
-//     for(auto it : Result){
-//         cout<<it<<" , ";
-//     }
-//     return 0 ;
-// }
+    for(auto it : Result){
+        cout<<it<<" , ";
+    }
+    return 0 ;
+}
