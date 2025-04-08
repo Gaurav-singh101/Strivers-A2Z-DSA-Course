63bdc26079aad74db7baab941bb99cd7f1748484
Gaurav101 <gauravsingh74580@gmail.com>
2025-04-08 23:33:49 +0530

    Lab Question

---
+++ Lab Exam/Insertionsort.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+void insertionsort(vector<int>& arr){
+    int n = arr.size();
+
+    for(int i = 1 ; i < n ; i++){
+        int key = arr[i];
+        int j = i - 1 ;
+
+        while(j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j-- ;
+        }
+
+        arr[j + 1] = key ;
+    }
+}
+
+int main(){
+
+
+    vector<int> arr = {4 , 9 , 3 , 19 , 0};
+      
+    insertionsort(arr);
+
+    cout<<"Sorted Array : ";
+
+    for(int i = 0 ; i < arr.size() ; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0 ;
+}
---
+++ Lab Exam/StrassenMatrix.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+vector<vector<int>> Multi(vector<vector<int>>& M1 , vector<vector<int>>& M2){
+
+    int m1 = (M1[0][0] + M1[1][1]) * (M2[0][0] + M2[1][1]);
+    int m2 = (M1[1][0] + M1[1][1]) * M2[0][0] ; 
+    int m3 = M1[0][0] * (M2[0][1] - M2[1][1]) ;
+    int m4 = M1[1][1] * (M2[1][0] - M2[0][0]) ;
+    int m5 = (M1[0][1] + M1[0][0]) * M2[1][1] ; 
+    int m6 = (M1[1][0] - M1[0][0]) * (M2[0][0] + M2[0][1]);
+    int m7 = (M1[0][1] - M1[1][1]) * (M2[1][0] + M2[1][1]);
+
+    int c1 = (m1 + m4) - (m5 + m7);
+    int c2 = m3 + m5 ;
+    int c3 = m2 + m4 ;
+    int c4 = (m1 + m3) + (m6 - m2);
+
+    vector<vector<int>> c ;
+
+    c[0][0] = c1 ; 
+    c[0][1] = c2 ; 
+    c[1][0] = c3 ; 
+    c[1][1] = c4 ;
+
+    return c ;
+
+}
+
+int main(){
+
+    vector<vector<int>> Mat1(3);
+    vector<vector<int>> Mat2(3);
+
+    cout<<"Input First Matrix : "<<endl;
+
+    for(int i = 0 ; i < 3 ; i++){
+        for(int j = 0 ; j < 3 ; j++){
+            cin>>Mat1[i][j];
+        }
+        cout<<endl ;
+    }
+
+    cout<<endl;
+
+    cout<<"Input Second Matrix : "<<endl ;
+    for(int i = 0 ; i < 3 ; i++){
+        for(int j = 0 ; j < 3 ; j++){
+            cin>>Mat2[i][j];
+        }
+        cout<<endl ;
+    }
+
+    vector<vector<int>> Result = Multi(Mat1 , Mat2);
+
+    for(int i = 0 ; i < 3 ; i++){
+        for(int j = 0 ; j < 3 ; j++){
+            cout<<Result[i][j]<<" ";
+        }
+        cout<<endl ;
+    }
+
+
+    return 0 ;
+}
