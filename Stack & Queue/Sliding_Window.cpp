c299ca705f7d79f6cae7243af200fd1c1c22940c
Gaurav101 <gauravsingh74580@gmail.com>
2025-06-28 00:18:15 +0530

    Stack and Queue

--- Stack & Queue/Sliding_Window.cpp
+++ Stack & Queue/Sliding_Window.cpp
@@ -21,6 +21,7 @@
 
 
 
+/////////////////  Brute Force ////////////////////
 
 #include<bits/stdc++.h>
 using namespace std ;
@@ -57,3 +58,55 @@ int main(){
 
     return 0 ;
 }
+
+
+
+
+
+//////////////////////  Optimal Solution  //////////////////////
+
+// #include<bits/stdc++.h>
+// using namespace std ;
+
+
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         vector<int> res ;
+//         deque<int> dq;
+
+//         for(int i = 0 ; i < nums.size() ; i++){
+//             if(!dq.empty() && dq.front() <= (i - k)){
+//                 dq.pop_front();
+//             }
+
+//             while(!dq.empty() && nums[dq.back()] <= nums[i]){
+//                 dq.pop_back();
+//             }
+
+//             dq.push_back(i);
+
+//             if(i >= k - 1) res.push_back(nums[dq.front()]);
+
+//         }
+
+//         return res ;
+//     }
+// };
+
+
+// int main(){
+
+//     vector<int> nums = {1,3,-1,-3,5,3,6,7} ;
+//     int k = 3 ;
+
+//     Solution obj ; 
+
+//     vector<int> res = obj.maxSlidingWindow(nums , k);
+
+//     for(auto it : res){
+//         cout<<it<<" , ";
+//     }
+
+//     return 0 ;
+// }
